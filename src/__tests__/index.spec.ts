3895217ba3eeacc00156839636d6efb02a857a2f
jinc.cjc <jinc.cjc@alibaba-inc.com>
2020-12-08 17:07:02 +0800

    feat: first

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,137 @@
+import PagingCore, { ILoader } from '../index'
+interface IData {
+  list: Array<string>
+  number: number
+  params?: Partial<{ name: string }>
+}
+const loader: ILoader<Partial<{ name: string }>, IData> = async (
+  number,
+  params
+) => {
+  return {
+    list: ['1', '2', '3'],
+    number,
+    params,
+  }
+}
+describe(`Greeter`, () => {
+  beforeEach(() => {})
+  it(`[NEW]:normal`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    let [data, err]: [IData | void, Error | void] = await paging.load()
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 1,
+      params: { name: 'jinc' },
+    })
+    expect(err).toBe(undefined)
+  })
+  it(`[API]:setParam`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    paging.setParam('name', 'jinci')
+    let [data, err]: [IData | void, Error | void] = await paging.load()
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 1,
+      params: { name: 'jinci' },
+    })
+    expect(err).toBe(undefined)
+  })
+  it(`[API]:setPage`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    paging.setPage(3)
+    let [data, err]: [IData | void, Error | void] = await paging.load()
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 3,
+      params: { name: 'jinc' },
+    })
+    expect(err).toBe(undefined)
+  })
+  it(`[API]:setParams`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    paging.setParams({
+      name: 'jinci',
+    })
+    let [data, err]: [IData | void, Error | void] = await paging.load()
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 1,
+      params: { name: 'jinci' },
+    })
+    expect(err).toBe(undefined)
+  })
+  it(`[API]:next`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    paging.next()
+    let [data, err]: [IData | void, Error | void] = await paging.load()
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 2,
+      params: { name: 'jinc' },
+    })
+    expect(err).toBe(undefined)
+  })
+  it(`[API]:last`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    paging.next()
+    paging.last()
+    let [data, err]: [IData | void, Error | void] = await paging.load()
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 1,
+      params: { name: 'jinc' },
+    })
+    expect(err).toBe(undefined)
+  })
+  it(`[API]:reset`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    paging.setParam('name', 'jinci')
+    paging.reset()
+    let [data, err]: [IData | void, Error | void] = await paging.load()
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 1,
+      params: { name: 'jinc' },
+    })
+    expect(err).toBe(undefined)
+  })
+  it(`[API]:getPageData`, async () => {
+    const paging = new PagingCore<Partial<{ name: string }>, IData>({
+      name: 'jinc',
+    })
+    paging.registered(loader)
+    await paging.load()
+    paging.next()
+    await paging.load()
+    paging.next()
+    await paging.load()
+    let data: IData | void = paging.getPageData(3)
+    expect(data).toEqual({
+      list: ['1', '2', '3'],
+      number: 3,
+      params: { name: 'jinc' },
+    })
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,69 @@
-export { Greeter } from './greeter'
+export interface ILoader<P, D> {
+  (page: number, params?: P): Promise<D>
+}
+export default class PagingCore<PARAMS, DATA> {
+  constructor(initParams?: PARAMS) {
+    if (!initParams) return
+    try {
+      this.initParams = JSON.parse(JSON.stringify(initParams))
+      this.params = JSON.parse(JSON.stringify(initParams))
+    } catch (error) {
+      console.warn(
+        `[PagingCore warn]: 'InitParams' copy error! Check your 'InitParams' is a Object type!`
+      )
+    }
+  }
+  private cacheData: Array<DATA | void> = []
+  private params: PARAMS | undefined
+  private initParams: PARAMS | undefined
+  private pageNumber: number = 1
+  private loader: ILoader<PARAMS, DATA> = async () => {
+    console.warn(
+      '[PagingCore warn]:Not registered the Loader, use this.registered(fn) method!'
+    )
+    return ([] as unknown) as DATA
+  }
+  registered(fn: ILoader<PARAMS, DATA>) {
+    this.loader = fn
+  }
+  async load(): Promise<[DATA | void, Error | void]> {
+    let data, err
+    try {
+      data = await this.loader(this.pageNumber, this.params)
+    } catch (error) {
+      err = error
+    }
+    this.cacheData[this.pageNumber] = data
+    return [data, err]
+  }
+  setPage(number: number) {
+    this.pageNumber = number
+  }
+  setParam(key: keyof PARAMS, value: PARAMS[keyof PARAMS]) {
+    if (!this.params) {
+      this.params = ({
+        [key]: value,
+      } as unknown) as PARAMS
+    } else {
+      this.params[key] = value
+    }
+  }
+  next() {
+    this.pageNumber += 1
+  }
+  last() {
+    this.pageNumber -= 1
+  }
+  setParams(params: PARAMS) {
+    this.params = params
+  }
+  reset() {
+    if (this.initParams) this.setParams(this.initParams)
+    else {
+      this.params = undefined
+    }
+  }
+  getPageData(page: number): DATA | void {
+    return this.cacheData[page]
+  }
+}
